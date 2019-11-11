b52404ab396e3c5dab058bca85a2ddc40321dc5a
Rian Perassoli <rian@torfresma.com.br>
2019-11-11 16:57:58 -0300

    Criada a tela de detalhe do produto

--- huntweb/src/pages/product/index.js
+++ huntweb/src/pages/product/index.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
+import api from '../../services/api'
+import { Link } from 'react-router-dom'
+
+import './styles.css'
 
 export default class index extends Component {
+
+    state = {
+        product: {},
+    }
+
+    async componentDidMount() {
+        const { id } = this.props.match.params
+
+        const response = await api.get(`/products/${id}`)
+
+        this.setState({ product: response.data })
+    }
+
     render() {
+        const { product } = this.state
+
         return (
-            <h1>Product</h1>
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+                <p>
+                    URL: <a href={product.url}>{product.url}</a>
+                </p>            
+            </div>            
         )
     }
 }
