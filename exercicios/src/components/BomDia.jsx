555667840d64f62c66376a24c02fbae49979129e
Rian Perassoli <rian@torfresma.com.br>
2019-11-12 16:20:06 -0300

    Criado Projeto e exercicios de componentes

---
+++ exercicios/src/components/BomDia.jsx
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react'
+
+export default (props) =>
+    <Fragment>
+        <h1>{props.nome}</h1>
+        <h1>Tchau </h1>
+    </Fragment> 
---
+++ exercicios/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/BomDia'
+
+ReactDOM.render(<App nome="Rian" />, document.getElementById('root'))
