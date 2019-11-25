f00e0fb3b86b3e3b753b62604bf956e615175605
Rian Perassoli <rian@torfresma.com.br>
2019-11-25 16:11:04 -0300

    Alteracoes

--- router-auth/src/routes.js
+++ router-auth/src/routes.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { isAuthenticated } from './auth'
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route 
+    <Route
         {...rest}
-        render={props => isAuthenticated() ? (<Component {...props} />) : <Redirect to={{pathname:"/login", state:{from: props.location} }} />}
+        render={props => isAuthenticated()
+            ? <Component {...props} />
+            : <Redirect to={{ pathname: "/login", state: { from: props.location } }} />}
     />
 )
 
