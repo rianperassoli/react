e912cbd830c20e7cf5416eeedadcb44f3e0bbd12
Rian Perassoli <rian@torfresma.com.br>
2019-11-25 16:46:10 -0300

    Criada rota not found

--- auth-app/src/components/Routes.js
+++ auth-app/src/components/Routes.js
@@ -4,6 +4,7 @@ import { Router, Switch, Route } from 'react-router'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
+import NotFound from './NotFound'
 
 import { history } from '../history'
 
@@ -13,6 +14,7 @@ const Routes = () => (
             <Route component={Login} exact path="/login" />
             <Route component={Register} exact path="/register" />
             <Route component={Home} exact path="/" />
+            <Route component={NotFound} />
         </Switch>
     </Router>
 )
