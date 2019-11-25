9219aac6878f141e46d98de9c4c59f5d43c49d87
Rian Perassoli <rian@torfresma.com.br>
2019-11-25 16:42:38 -0300

    Criado projeto e rotas do mesmo

---
+++ auth-app/src/components/Routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Router, Switch, Route } from 'react-router'
+
+import Register from '../pages/Register'
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+
+import { history } from '../history'
+
+const Routes = () => (
+    <Router history={history}>
+        <Switch>
+            <Route component={Login} exact path="/login" />
+            <Route component={Register} exact path="/register" />
+            <Route component={Home} exact path="/" />
+        </Switch>
+    </Router>
+)
+
+export default Routes
