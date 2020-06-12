1e7f71a972a91dc43881a5f77602f8ca5bd20b9a
Rian Perassoli <rian@torfresma.com.br>
2020-06-12 15:45:53 -0300

    Criada uma Todo list

--- drag-and-drop-test-example/src/index.js
+++ drag-and-drop-test-example/src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+import '@atlaskit/css-reset'
 
-const App = () => 'Empty Project'
+import InitialData from './initial-data'
+import Column from './column'
+
+const App = () => {
+
+  const [boardItems] = useState(InitialData)
+
+  return (
+    boardItems.columnOrder.map(columnId => {
+      const column = boardItems.columns[columnId]
+      const tasks = column.taskIds.map(taskId => boardItems.tasks[taskId])
+
+      return <Column key={column.id} column={column} tasks={tasks} />
+    })
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
