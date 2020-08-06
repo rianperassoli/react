5184d9f9ef69c6c43a82af8555d3176facab1d68
Rian Perassoli <rian@torfresma.com.br>
2020-08-06 10:23:01 -0300

    Adicionado react-tabs ao projeto

--- simple-tabs-example/src/App.jsx
+++ simple-tabs-example/src/App.jsx
@@ -1,31 +1,61 @@
 import React from 'react';
 import Tabs from './Tabs';
 import Tab from './Tab';
+import { Tab as TabReactTabs, Tabs as TabsReactTabs, TabList, TabPanel } from 'react-tabs';
 
+import 'react-tabs/style/react-tabs.css';
 import './styles.css'
 
 const App = () => {
   return (
-    <Tabs>
-      <Tab label={'Tab 1'}>
-        <p>Content 1</p>
-      </Tab>
+    <>
+      <TabsReactTabs>
+        <TabList>
+          <TabReactTabs>Title 1</TabReactTabs>
+          <TabReactTabs>Title 2</TabReactTabs>
+          <TabReactTabs>Title 3</TabReactTabs>
+          <TabReactTabs>Title 4</TabReactTabs>
+        </TabList>
 
-      <Tab label={'Tab 2'}>
-        <ul>
-          <li>List</li>
-          <li>Content</li>
-          <li>2</li>
-        </ul>
-      </Tab>
+        <TabPanel>
+          <p>Content 1</p>
+        </TabPanel>
+        <TabPanel>
+          <ul>
+            <li>List</li>
+            <li>Content</li>
+            <li>2</li>
+          </ul>
+        </TabPanel>
+        <TabPanel>
+          <figure>
+            <img src={'http://placehold.it/200'} alt={'Image Content 3'} />
+            <figcaption>Image Content 3</figcaption>
+          </figure>
+        </TabPanel>
+      </TabsReactTabs>
 
-      <Tab label={'Tab 3'}>
-        <figure>
-          <img src={'http://placehold.it/200'} alt={'Image Content 3'} />
-          <figcaption>Image Content 3</figcaption>
-        </figure>
-      </Tab>
-    </Tabs>
+      <Tabs>
+        <Tab label={'Tab 1'}>
+          <p>Content 1</p>
+        </Tab>
+
+        <Tab label={'Tab 2'}>
+          <ul>
+            <li>List</li>
+            <li>Content</li>
+            <li>2</li>
+          </ul>
+        </Tab>
+
+        <Tab label={'Tab 3'}>
+          <figure>
+            <img src={'http://placehold.it/200'} alt={'Image Content 3'} />
+            <figcaption>Image Content 3</figcaption>
+          </figure>
+        </Tab>
+      </Tabs>
+    </>
   );
 }
 
